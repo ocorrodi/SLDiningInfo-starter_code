b2afa143da5f4936b84fd76be40dbf84b426d80c
ocorrodi <olivia.corrodi@gmail.com>
2020-11-13 23:17:50 -0500

    Initial Commit

---
+++ SLDiningInfo-starter_code/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SLDiningInfo-starter_code
+//
+//  Created by Olivia Corrodi on 11/13/20.
+//  Copyright © 2020 Olivia Corrodi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
